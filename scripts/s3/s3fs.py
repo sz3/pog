51f3240d572dda3fd7909cb4e91f23aef853c35b
sz <sz@galacticicecube.com>
2020-01-08 19:43:35 -0600

    Beginnings of the actual s3fs impl

--- scripts/s3/s3fs.py
+++ scripts/s3/s3fs.py
@@ -1,14 +1,32 @@
+from os import environ
+
 import boto3
+from botocore.exceptions import ClientError
 
 from ..pogfs import Pogfs
 
 
+BUCKET_NAME = environ.get('S3_BUCKET_NAME')
+
+
 class s3fs(Pogfs):
+    def __init__(self, bucket_name=None):
+        self.bucket_name = bucket_name or BUCKET_NAME
+
     def exists(self, remote_path):
-        raise NotImplementedError()
+        resource_s3 = boto3.resource('s3')
+        try:
+            resource_s3.Object(self.bucket_name, remote_path).load()
+            return True
+        except ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                return False
+            else:
+                raise
 
     def download_file(self, remote_path, local_path):
-        raise NotImplementedError()
+        s3 = boto3.client('s3')
+        s3.download_file(self.bucket_name, remote_path, local_path)
 
     def remove_file(self, remote_path):
         raise NotImplementedError()
