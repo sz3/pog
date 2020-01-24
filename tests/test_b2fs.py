7f257a6c9ee275cb70b9ff52ce5ebe7b1944e7b5
sz <sz@galacticicecube.com>
2020-01-24 06:38:31 -0600

    Add tests for the fssss

---
+++ tests/test_b2fs.py
@@ -0,0 +1,96 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from pog.fs.pogfs import get_cloud_fs
+
+
+EX_LIST_FILE_NAMES = b'''
+{
+  "files": [
+    {
+      "accountId": "aaabbbccc",
+      "action": "upload",
+      "bucketId": "aaabbbcccdddeeefff",
+      "contentLength": 1234,
+      "contentMd5": null,
+      "contentSha1": "0101020304",
+      "contentType": "application/octet-stream",
+      "fileId": "4_abcdef123456789_t099",
+      "fileInfo": {
+        "src_last_modified_millis": "1576098384516"
+      },
+      "fileName": "pog.py",
+      "uploadTimestamp": 1577746433000
+    }
+  ],
+  "nextFileName": "pog.py "
+}
+'''
+
+EX_LS = b'''
+data/
+file.txt
+pog.py
+'''
+
+EX_LS_RECURSIVE = b'''
+data/AA/AAaa0123456789=
+data/BB/BBbb0123456789=
+file.txt
+pog.py
+'''
+
+
+@patch('pog.fs.b2fs.check_output', autoSpec=True)
+class b2fsTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.fs = get_cloud_fs('b2')('bucket')
+
+    def test_exists(self, mock_run):
+        mock_run.return_value = EX_LIST_FILE_NAMES
+
+        self.assertFalse(self.fs.exists('foobar'))
+        self.assertEqual(self.fs.exists('foobar'), '')
+        self.assertTrue(self.fs.exists('pog.py'))
+        self.assertEqual(self.fs.exists('pog.py'), '4_abcdef123456789_t099')
+
+        mock_run.assert_any_call(['b2', 'list-file-names', 'bucket', 'foobar', '1'])
+        mock_run.assert_any_call(['b2', 'list-file-names', 'bucket', 'pog.py', '1'])
+
+    def test_upload_file(self, mock_run):
+        self.fs.upload_file('local', 'remote')
+        mock_run.assert_called_once_with(['b2', 'upload_file', 'bucket', 'local', 'remote'])
+
+    def test_download_file(self, mock_run):
+        self.fs.download_file('local', 'remote')
+        mock_run.assert_called_once_with(['b2', 'download-file-by-name', 'bucket', 'remote', 'local'])
+
+    def test_remove_file(self, mock_run):
+        self.fs.exists = MagicMock()
+        self.fs.exists.return_value = 'abc1234'
+
+        self.fs.remove_file('foobar')
+        mock_run.assert_called_once_with(['b2', 'delete-file-version', 'foobar', 'abc1234'])
+
+    def test_remove_file_does_not_exist(self, mock_run):
+        self.fs.exists = MagicMock()
+        self.fs.exists.return_value = ''
+
+        self.fs.remove_file('foobar')
+        self.assertEqual(mock_run.call_count, 0)
+
+    def test_list_files_defaults(self, mock_run):
+        mock_run.return_value = EX_LS_RECURSIVE
+        self.assertEqual(self.fs.list_files(), ['data/AA/AAaa0123456789=', 'data/BB/BBbb0123456789=', 'file.txt', 'pog.py'])
+        mock_run.assert_called_once_with(['b2', 'ls', '--recursive', 'bucket'])
+
+    def test_list_files_subdir(self, mock_run):
+        mock_run.return_value = EX_LS
+        self.assertEqual(self.fs.list_files('path/to/dir', recursive=False), ['data/', 'file.txt', 'pog.py'])
+        mock_run.assert_called_once_with(['b2', 'ls', 'bucket', 'path/to/dir'])
+
+    def test_list_files_empty(self, mock_run):
+        mock_run.return_value = b''
+        self.assertEqual(self.fs.list_files('path/to/nowhere', recursive=True), [])
+        mock_run.assert_called_once_with(['b2', 'ls', '--recursive', 'bucket', 'path/to/nowhere'])
---
+++ tests/test_s3fs.py
@@ -0,0 +1,92 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from botocore.exceptions import ClientError
+
+from pog.fs.pogfs import get_cloud_fs
+
+
+@patch('pog.fs.s3fs.boto3', autoSpec=True)
+class s3fsTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.fs = get_cloud_fs('s3')('bucket')
+
+    def test_exists(self, mock_boto):
+        mock_boto.resource.return_value = mock_boto
+        mock_boto.Object.return_value = mock_boto
+
+        self.assertTrue(self.fs.exists('foo'))
+
+        mock_boto.resource.assert_called_once_with('s3')
+        mock_boto.Object.assert_called_once_with('bucket', 'foo')
+        mock_boto.load.assert_called_once_with()
+
+    def test_exists_false(self, mock_boto):
+        mock_boto.resource.return_value = mock_boto
+        mock_boto.Object.return_value = mock_boto
+        mock_boto.load.side_effect = ClientError({'Error': {'Code': '404', 'Message': 'Not Found'}}, 'HeadObject')
+
+        self.assertFalse(self.fs.exists('foobar'))
+
+        mock_boto.resource.assert_called_once_with('s3')
+        mock_boto.Object.assert_called_once_with('bucket', 'foobar')
+        mock_boto.load.assert_called_once_with()
+
+    def test_exists_kaboom(self, mock_boto):
+        mock_boto.resource.return_value = mock_boto
+        mock_boto.Object.side_effect = Exception('onoes')
+        with self.assertRaises(Exception) as e:
+            self.fs.exists('uhoh')
+
+        self.assertEqual(str(e.exception), 'onoes')
+
+    def test_upload_file(self, mock_boto):
+        mock_boto.client.return_value = mock_boto
+
+        self.fs.upload_file('local', 'remote')
+
+        mock_boto.client.assert_called_once_with('s3')
+        mock_boto.upload_file.assert_called_once_with('local', 'bucket', 'remote')
+
+    def test_download_file(self, mock_boto):
+        mock_boto.client.return_value = mock_boto
+
+        self.fs.download_file('local', 'remote')
+
+        mock_boto.client.assert_called_once_with('s3')
+        mock_boto.download_file.assert_called_once_with('bucket', 'remote', 'local')
+
+    def test_remove_file(self, mock_boto):
+        mock_boto.client.return_value = mock_boto
+
+        self.fs.remove_file('remote')
+
+        mock_boto.client.assert_called_once_with('s3')
+        mock_boto.delete_object.assert_called_once_with(Bucket='bucket', Key='remote')
+
+    def test_list_files_defaults(self, mock_boto):
+        mock_boto.client.return_value = mock_boto
+        mock_boto.get_paginator.return_value = mock_boto
+        mock_boto.paginate.side_effect = [
+            [{'Contents': [{'Key': 'abc'}, {'Key': 'def'}]}],
+        ]
+
+        self.assertEqual(list(self.fs.list_files()), ['abc', 'def'])
+
+        mock_boto.client.assert_called_once_with('s3')
+        mock_boto.get_paginator.assert_called_once_with('list_objects_v2')
+        mock_boto.paginate.assert_called_once_with(Bucket='bucket', Prefix='')
+
+    def test_list_files_subdir(self, mock_boto):
+        mock_boto.client.return_value = mock_boto
+        mock_boto.get_paginator.return_value = mock_boto
+        mock_boto.paginate.side_effect = [
+            [{'Contents': [{'Key': 'abc'}, {'Key': 'def'}]}],
+        ]
+
+        self.assertEqual(list(self.fs.list_files('path/to/files', recursive=False)), ['abc', 'def'])
+
+        mock_boto.client.assert_called_once_with('s3')
+        mock_boto.get_paginator.assert_called_once_with('list_objects_v2')
+        mock_boto.paginate.assert_called_once_with(Bucket='bucket', Prefix='path/to/files', Delimiter='/')
